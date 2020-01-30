e51761241a0b36ca415abb4693f77403f2961854
Bushway <Nick.Bushway@us.sogeti.com>
2020-01-30 10:22:14 -0500

    Convert react app from class based structure to hook structure

--- src/App.js
+++ src/App.js
@@ -1,50 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [employees, setEmployees] = React.useState([])
 
-    this.state = {
-      Employees: []
-    }
-  }
-
-  componentDidMount() {
+  React.useEffect(() => {
     fetch('http://localhost:3500/getEmployees')
       .then(Employees => Employees.json())
       .then(_employees => {
         console.log(_employees)
-        this.setState({
-          Employees: _employees
-        })
+        setEmployees(_employees)
       })
-  }
+      .catch(err => console.err(err))
+  });
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Sogeti Employee Directory</h1>
-        <div className="container">
-          <div className="row">
-            {this.state.Employees.map((employee, index) => {
-              return (
-                <div class="card" style={{ width: '18rem' }} key={index}>
-                  <img src={employee.imageUrl} style={{ padding: '10px' }} class="card-img-top"></img>
-                  <div className="card-body">
-                    <h3 className="card-body">{employee.name}</h3>
-                    <p className="card-text">{employee.location}</p>
-                    <p className="card-text">{employee.cellNumber}</p>
-                  </div>
+  return (
+    <div className="App">
+      <h1>Sogeti Employee Directory</h1>
+      <div className="container">
+        <div className="row">
+          {employees.map((employee, index) => {
+            return (
+              <div class="card" style={{ width: '18rem' }} key={index}>
+                <img src={employee.imageUrl} style={{ padding: '10px' }} class="card-img-top"></img>
+                <div className="card-body">
+                  <h3 className="card-body">{employee.name}</h3>
+                  <p className="card-text">{employee.location}</p>
+                  <p className="card-text">{employee.cellNumber}</p>
                 </div>
-              )
-            })}
-          </div>
+              </div>
+            )
+          })}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App;
